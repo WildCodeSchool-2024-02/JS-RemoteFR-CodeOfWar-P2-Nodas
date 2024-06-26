0925684c1dff3b5e5d6523d742b2797d1453ec47
Your Name <ewan16270409@outlook.fr>
2024-06-26 17:26:40 +0200

    Ajout fonction axios avec une route test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
-  return <h1>TITRE</h1>;
+  const [infosjeu, setInfosjeu] = useState([]);
+  const fetchData = () => {
+    axios
+      .get(
+        "https://api.rawg.io/api/games/3498?key=528f2e0850a34934a043d35c3ece72a9"
+      )
+      .then((response) => setInfosjeu(response.data))
+      .catch((error) => console.error(error));
+  };
+  return (
+    <>
+      <h1>{infosjeu.name}</h1>
+      <button type="button" onClick={fetchData}>
+        Fetch
+      </button>
+    </>
+  );
 }
 
 export default App;
