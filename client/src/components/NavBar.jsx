7d071647e88e2b138c88c52449dd633ae7b97ae9
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-06-26 10:39:37 +0200

    renamed images, added menuburger image to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,13 @@
-import logoNG from "../assets/images/logoNG.svg";
+import logong from "../assets/images/logong.svg";
+import menuburger from "../assets/images/menuburger.svg";
 
 export default function NavBar() {
   return (
     <nav>
       <a href="/burger" className="menu-burger" aria-label="Menu burger">
-        <img src="../assets/images/" alt="" />
+        <img src={menuburger} alt="Menu burger" />
       </a>
-      <img src={logoNG} alt="Logo" />
+      <img src={logong} alt="Logo" />
       <a href="/cart">
         <img src="#" alt="Logo panier" />
       </a>
