05f6efdc058ba6aea09bd49ca767748e8f00625c
Your Name <ewan16270409@outlook.fr>
2024-06-26 17:30:25 +0200

    Add composant CategorieItem

---
+++ client/src/components/CategorieItem.jsx
@@ -0,0 +1,12 @@
+export default function CategorieItem() {
+  return (
+    <>
+      <img
+        src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2344520/capsule_616x353.jpg?t=1719248064"
+        alt="Diablo 4"
+      />
+      <h3>Diablo 4</h3>
+      <h3>99€</h3>
+    </>
+  );
+}
