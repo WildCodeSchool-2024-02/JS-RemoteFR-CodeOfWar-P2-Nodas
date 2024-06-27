ec87f49450cb10b9cc0a20f8aad0833101114c58
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-06-27 15:54:44 +0200

    added search icon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import logong from "../assets/images/logong.svg";
 import menuburger from "../assets/images/menuburger.svg";
 import paniericon from "../assets/images/paniericon.svg";
+import searchicon from "../assets/images/searchicon.svg";
 
 export default function NavBar() {
   return (
     <nav>
-      <a href="/burger" className="menu-burger" aria-label="Menu burger">
-        <img src={menuburger} alt="Menu burger" />
-      </a>
       <img src={logong} alt="Logo" />
+      <a href="/search">
+        <img src={searchicon} alt="Logo recherche" />
+      </a>
       <a href="/cart">
         <img src={paniericon} alt="Logo panier" />
+      </a>
+      <a href="/burger" className="menu-burger" aria-label="Menu burger">
+        <img src={menuburger} alt="Menu burger" />
       </a>
     </nav>
   );
