55a4f4039d2253ef6caeb998c0ea41b3153faa98
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-06-27 21:34:11 +0200

    created carrousel component and CSS, added carrousel logic to use pictures from external objects

--- client/src/pages/Accueil.jsx
+++ client/src/pages/Accueil.jsx
@@ -1,53 +1,64 @@
+import CarrouselStd from "../components/CarrouselStd";
+
 export default function Accueil() {
- return (
+  const featuredImages = [
+    { id: 1, url: "https://picsum.photos/400/250?random=1"},
+    { id: 2, url: "https://picsum.photos/400/250?random=2"},
+    { id: 3, url: "https://picsum.photos/400/250?random=3"},
+  ];
+  const categorieImages = [
+    { id: 4, url: "https://picsum.photos/400/250?random=4"},
+    { id: 5, url: "https://picsum.photos/400/250?random=5"},
+    { id: 6, url: "https://picsum.photos/400/250?random=6"},
+  ];
+  const salesImages = [
+    { id: 7, url: "https://picsum.photos/400/250?random=7"},
+    { id: 8, url: "https://picsum.photos/400/250?random=8"},
+    { id: 9, url: "https://picsum.photos/400/250?random=9"},
+  ];
+  return (
     <>
-    <div>
+      <div>
         <h1>BANNER</h1>
-    </div>
-    <div>
+      </div>
+      <div>
         <h2>Featured games</h2>
-        <p>Featured games, criteria = high sales, high rating, less than 3 month</p>
-        <img src="https://picsum.photos/400/250" alt="Jaquette de jeu" />
-        <img src="https://picsum.photos/400/250" alt="Jaquette de jeu" />
-        <img src="https://picsum.photos/400/250" alt="Jaquette de jeu" />
-    </div>
-    <div>
+        <p>
+          Featured games, criteria = high sales, high rating, less than 3 month
+        </p>
+        <CarrouselStd images={featuredImages} />
+      </div>
+      <div>
         <h2>Categorie</h2>
         <p>Random categorie with some cards</p>
-        <img src="https://picsum.photos/400/300" alt="Jaquette de jeu categorie" />
-        <img src="https://picsum.photos/400/300" alt="Jaquette de jeu categorie" />
-        <img src="https://picsum.photos/400/300" alt="Jaquette de jeu categorie" />
-    </div>
-    <div>
+        <CarrouselStd images={categorieImages} />
+      </div>
+      <div>
         <h2>Sales of the moment</h2>
         <p>Featured sales with some game cards and reduced prices</p>
-        <img src="https://picsum.photos/350/200" alt="" />
-        <img src="https://picsum.photos/350/200" alt="" />
-        <img src="https://picsum.photos/350/200" alt="" />
-    </div>
-    <div>
+        <CarrouselStd images={salesImages} />
+      </div>
+      <div>
         <h2>Search engine</h2>
         <p>Main search and filter engine with results in pages</p>
         <div>
-            <img src="https://picsum.photos/200/130" alt="" />
-            <h3>Game name</h3>
-            <p>Game details</p>
-            <img src="https://picsum.photos/200/130" alt="" />
-            <h3>Game name</h3>
-            <p>Game details</p>
-            <img src="https://picsum.photos/200/130" alt="" />
-            <h3>Game name</h3>
-            <p>Game details</p>
-            <img src="https://picsum.photos/200/130" alt="" />
-            <h3>Game name</h3>
-            <p>Game details</p>
-            <img src="https://picsum.photos/200/130" alt="" />
-            <h3>Game name</h3>
-            <p>Game details</p>
+          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
+          <h3>Game name</h3>
+          <p>Game details</p>
+          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
+          <h3>Game name</h3>
+          <p>Game details</p>
+          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
+          <h3>Game name</h3>
+          <p>Game details</p>
+          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
+          <h3>Game name</h3>
+          <p>Game details</p>
+          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
+          <h3>Game name</h3>
+          <p>Game details</p>
         </div>
-    </div>
-    
+      </div>
     </>
-
- )
+  );
 }
