cc512e7dcd99872ec26b4dee72308e29c87d96e7
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-06-27 19:15:11 +0200

    created basic landing page structure with examples and plain text, images

--- client/src/pages/Accueil.jsx
+++ client/src/pages/Accueil.jsx
@@ -1,5 +1,53 @@
 export default function Accueil() {
  return (
-    <h1>Coucou</h1>
+    <>
+    <div>
+        <h1>BANNER</h1>
+    </div>
+    <div>
+        <h2>Featured games</h2>
+        <p>Featured games, criteria = high sales, high rating, less than 3 month</p>
+        <img src="https://picsum.photos/400/250" alt="Jaquette de jeu" />
+        <img src="https://picsum.photos/400/250" alt="Jaquette de jeu" />
+        <img src="https://picsum.photos/400/250" alt="Jaquette de jeu" />
+    </div>
+    <div>
+        <h2>Categorie</h2>
+        <p>Random categorie with some cards</p>
+        <img src="https://picsum.photos/400/300" alt="Jaquette de jeu categorie" />
+        <img src="https://picsum.photos/400/300" alt="Jaquette de jeu categorie" />
+        <img src="https://picsum.photos/400/300" alt="Jaquette de jeu categorie" />
+    </div>
+    <div>
+        <h2>Sales of the moment</h2>
+        <p>Featured sales with some game cards and reduced prices</p>
+        <img src="https://picsum.photos/350/200" alt="" />
+        <img src="https://picsum.photos/350/200" alt="" />
+        <img src="https://picsum.photos/350/200" alt="" />
+    </div>
+    <div>
+        <h2>Search engine</h2>
+        <p>Main search and filter engine with results in pages</p>
+        <div>
+            <img src="https://picsum.photos/200/130" alt="" />
+            <h3>Game name</h3>
+            <p>Game details</p>
+            <img src="https://picsum.photos/200/130" alt="" />
+            <h3>Game name</h3>
+            <p>Game details</p>
+            <img src="https://picsum.photos/200/130" alt="" />
+            <h3>Game name</h3>
+            <p>Game details</p>
+            <img src="https://picsum.photos/200/130" alt="" />
+            <h3>Game name</h3>
+            <p>Game details</p>
+            <img src="https://picsum.photos/200/130" alt="" />
+            <h3>Game name</h3>
+            <p>Game details</p>
+        </div>
+    </div>
+    
+    </>
+
  )
 }
