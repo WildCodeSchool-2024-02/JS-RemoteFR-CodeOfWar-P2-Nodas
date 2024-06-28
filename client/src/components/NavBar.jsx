4375ef9b64944560f8949cf53a30124d9e4edfd2
Kevin Peset <kevin.peset@gmail.com>
2024-06-28 09:47:57 +0200

    Resolve logo path

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import logong from "../assets/images/logong.svg";
+import logong from "../assets/images/logoNG.svg";
 import menuburger from "../assets/images/menuburger.svg";
 import paniericon from "../assets/images/paniericon.svg";
 import searchicon from "../assets/images/searchicon.svg";
@@ -6,21 +6,21 @@ import searchicon from "../assets/images/searchicon.svg";
 export default function NavBar() {
   return (
     <>
-    <nav className="Navbar">
-      <img src={logong} alt="Logo" />
-      <div className="iconsGroup">
-      <a href="/search">
-        <img src={searchicon} alt="Logo recherche" />
-      </a>
-      <a href="/cart">
-        <img src={paniericon} alt="Logo panier" />
-      </a>
-      <a href="/burger" className="menu-burger" aria-label="Menu burger">
-        <img src={menuburger} alt="Menu burger" />
-      </a>
-      </div>
-    </nav>
-    <div className="animated-border" />
+      <nav className="Navbar">
+        <img src={logong} alt="Logo" />
+        <div className="iconsGroup">
+          <a href="/search">
+            <img src={searchicon} alt="Logo recherche" />
+          </a>
+          <a href="/cart">
+            <img src={paniericon} alt="Logo panier" />
+          </a>
+          <a href="/burger" className="menu-burger" aria-label="Menu burger">
+            <img src={menuburger} alt="Menu burger" />
+          </a>
+        </div>
+      </nav>
+      <div className="animated-border" />
     </>
   );
 }
