c923f867df0a92aacd68f15797638716c6a569e2
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-06-28 18:41:01 +0200

    basic style applied to landing page (accueil) with sections clearly defined

--- client/src/pages/Accueil.jsx
+++ client/src/pages/Accueil.jsx
@@ -1,4 +1,5 @@
 import CarrouselStd from "../components/CarrouselStd";
+import GameCardAccueil from "../components/GameCardAccueil"
 
 export default function Accueil() {
   const featuredImages = [
@@ -17,48 +18,38 @@ export default function Accueil() {
     { id: 9, url: "https://picsum.photos/400/250?random=9"},
   ];
   return (
-    <>
+    <div className="landing-page">
+    <header>
       <div>
         <h1>BANNER</h1>
       </div>
-      <div>
+      </header>
+      <main>
+      <section className="featured-games">
         <h2>Featured games</h2>
         <p>
           Featured games, criteria = high sales, high rating, less than 3 month
         </p>
         <CarrouselStd images={featuredImages} />
-      </div>
-      <div>
+      </section>
+      <section className="featured-categorie">
         <h2>Categorie</h2>
         <p>Random categorie with some cards</p>
         <CarrouselStd images={categorieImages} />
-      </div>
-      <div>
+      </section>
+      <section className="featured-sales">
         <h2>Sales of the moment</h2>
         <p>Featured sales with some game cards and reduced prices</p>
         <CarrouselStd images={salesImages} />
-      </div>
-      <div>
+      </section>
+      <section className="search-engine">
         <h2>Search engine</h2>
         <p>Main search and filter engine with results in pages</p>
         <div>
-          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
-          <h3>Game name</h3>
-          <p>Game details</p>
-          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
-          <h3>Game name</h3>
-          <p>Game details</p>
-          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
-          <h3>Game name</h3>
-          <p>Game details</p>
-          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
-          <h3>Game name</h3>
-          <p>Game details</p>
-          <img src="https://picsum.photos/200/130" alt="Game screenshot" />
-          <h3>Game name</h3>
-          <p>Game details</p>
+          <GameCardAccueil />
         </div>
-      </div>
-    </>
+      </section>
+      </main>
+    </div>
   );
 }
