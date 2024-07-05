f45e62ecbd767f28a48c5cd894cc50613418e236
Rosa Ben <benchabane.rosa@outlook.com>
2024-07-05 23:47:44 +0200

    corrected carousel random function and moved it ot utils.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,6 +25,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Accueil />,
+        loader: fetchData,
       },
       {
         path: "/catalog",
--- client/src/pages/Accueil.jsx
+++ client/src/pages/Accueil.jsx
@@ -1,25 +1,30 @@
+import { useLoaderData } from "react-router-dom";
 import CarrouselStd from "../components/CarrouselStd";
+import getRandomGames from "../services/utils";
 
 export default function Accueil() {
-  const featuredImages = [
-    { id: 1, url: "https://picsum.photos/400/250?random=1"},
-    { id: 2, url: "https://picsum.photos/400/250?random=2"},
-    { id: 3, url: "https://picsum.photos/400/250?random=3"},
-  ];
-  const salesImages = [
-    { id: 7, url: "https://picsum.photos/400/250?random=7"},
-    { id: 8, url: "https://picsum.photos/400/250?random=8"},
-    { id: 9, url: "https://picsum.photos/400/250?random=9"},
-  ];
+  const gamesDetails = useLoaderData();
+  const numberOfRandomGames = 5;
+  const featuredGames = getRandomGames(gamesDetails, numberOfRandomGames);
+  const salesGames = getRandomGames(gamesDetails, numberOfRandomGames);
+  const featuredImages = featuredGames.map((game) => ({
+    id: game.id,
+    name: game.name,
+    url: game.background_image,
+  }));
+  const salesImages = salesGames.map((game) => ({
+    id: game.id,
+    name: game.name,
+    url: game.background_image,
+  }));
+
   return (
     <div className="landing-page">
       <h1 className="landing-page-title">Welcome to Noda Games</h1>
       <section className="featured-games">
         <h2>Featured games</h2>
         <div className="decobar" />
-        <p>
-          Featured games, high sales, high rating
-        </p>
+        <p>Featured games, high sales, high rating</p>
         <CarrouselStd images={featuredImages} />
       </section>
       <section className="featured-sales">
@@ -31,8 +36,13 @@ export default function Accueil() {
       <h2>Still hungry for more ?</h2>
       <section className="search-engine">
         <h2>Explore all possibilities in our endless catalog</h2>
-        <p>All the games you ever wanted to play are here, all platforms, all genres. Take a look and choose your next adventure !</p>
-        <button type="button" id="catalog-button">See all games</button>
+        <p>
+          All the games you ever wanted to play are here, all platforms, all
+          genres. Take a look and choose your next adventure !
+        </p>
+        <button type="button" id="catalog-button">
+          See all games
+        </button>
       </section>
     </div>
   );
