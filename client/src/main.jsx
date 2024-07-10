720fcd6b33ddc574c71216a68756bd29a8d95b4a
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-07-10 15:44:23 +0200

    in page catalogue added aside html filter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         loader: fetchData,
       },
       {
-        path: "/catalog",
+        path: "/catalogue",
         element: <Catalogue />,
       },
       {
--- client/src/pages/Catalogue.jsx
+++ client/src/pages/Catalogue.jsx
@@ -1,3 +1,26 @@
 export default function Catalogue() {
-  return <h1>laredoute</h1>;
+  return (
+    <section className="filter_catalogue">
+      <h2>Filtrer les jeux par :</h2>
+      <button type="button">Réinitiliser les filtres</button>
+      <p>CHERCHER UN JEU</p>
+      <input type="text" name="Nom du jeu" />
+      <div className="label_search">
+        <label htmlFor="select-genre">GENRE</label>
+        <select name="genre">
+          <option value="">--choisir un genre</option>
+        </select>
+
+        <label htmlFor="select-genre">PLATFORME</label>
+        <select name="platform">
+          <option value="">--choisir une platforme</option>
+        </select>
+
+        <label htmlFor="select-genre">DEVELOPPERS</label>
+        <select name="developpers">
+          <option value="">--choisir un developper</option>
+        </select>
+      </div>
+    </section>
+  );
 }
