4f09ad8be81e30aa5ee05c35bfcd669b79901039
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-07-10 17:58:12 +0200

    modification on Footer.jsx and the previousPage working

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import previous from "../assets/images/previous.png";
 import home from "../assets/images/home.png";
 import favorite from "../assets/images/favorite.png";
 
 export default function Footer() {
+  const navigate = useNavigate();
+  const historyBack = () => {
+    navigate(-1);
+  };
   return (
     <footer className="Footer">
       <section className="iconsFoot">
-        <Link className="iconPrevious" to="/">
-          <img src={previous} alt="Previous" />
-        </Link>
-
+        <div>
+          <button onClick={historyBack} type="button">
+            <img src={previous} alt="Previous" />
+          </button>
+        </div>
         <Link className="iconHome" to="/">
           <img src={home} alt="Home" />
         </Link>
