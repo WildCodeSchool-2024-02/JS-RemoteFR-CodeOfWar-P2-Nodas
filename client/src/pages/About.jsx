369efc948291a4ea1f3e32ac73982d141e99cefc
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-07-15 16:22:03 +0200

    add file CardTeam.jsx relation with About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 import SocialNetwork from "../components/SocialNetwork";
 import gamer from "../assets/images/gamer.png";
-import team from "../data/teamData";
+import CardTeam from "../components/CardTeam";
 
 export default function About() {
   return (
@@ -26,28 +25,7 @@ export default function About() {
         combined their skills to design and implement an exceptional user
         experience. Together, they are redefining video game discovery!
       </p>
-      <section className="avatar">
-        {team.map((user) => (
-          <div key={user.id} className="cardContainer">
-            <div>
-              <img src={user.avatar} alt={`Avatar ${user.name}`} />
-            </div>
-            <div className="userDetails">
-              <h3 id="about3">{user.name}</h3>
-              <ul className="listUser">
-                <li>Age : {user.age} years old</li>
-                <li>Description : {user.description}</li>
-                <li>
-                  jeu préféré :{" "}
-                  <Link to={user.pageUrl} target="_blank" rel="noreferrer">
-                    {user.favoriteGame}
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        ))}
-      </section>
+      <CardTeam />
       <h2 id="about2">Follow us</h2>
       <SocialNetwork />
       <p className="copyright">
