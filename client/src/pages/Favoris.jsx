c44a069784647bbf913252af95cc484cd7930df9
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-07-17 14:50:14 +0200

    fixed some probleme + add link to categories + link socialnetwork

--- client/src/pages/Favoris.jsx
+++ client/src/pages/Favoris.jsx
@@ -4,23 +4,23 @@ import FavorisItem from "../components/FavorisItem";
 import FavoriteContext from "../contexts/FavoriteContext";
 
 function Favoris() {
-  const {favoris} = useContext(FavoriteContext)
-  
-  return (
+  const { favoris } = useContext(FavoriteContext);
 
-      <section className="wishlist">
-        <h2>Liste de souhaits</h2>
-        {favoris.map((id, index) => (
-          <FavorisItem
-            key={[index]}
-            id={id}
-          />
-        ))}
-        {favoris.length > 0 ? "": <p className="no-fav-msg">Vous n'avez aucun jeu dans votre liste de souhaits.</p>}
-        <Link to="/catalog">
-        Continuer vers le catalogue...
-        </Link>
-      </section>
+  return (
+    <section className="wishlist">
+      <h2>Liste de souhaits</h2>
+      {favoris.map((id, index) => (
+        <FavorisItem key={[index]} id={id} />
+      ))}
+      {favoris.length > 0 ? (
+        ""
+      ) : (
+        <p className="no-fav-msg">
+          Vous n'avez aucun jeu dans votre liste de souhaits.
+        </p>
+      )}
+      <Link to="/categories">Continuer vers les categories...</Link>
+    </section>
   );
 }
 
