80f668fc35bec0e1d1e366b2f45457c180cef5e7
Kevin Peset <97118056+kpeset@users.noreply.github.com>
2024-07-17 15:03:00 +0200

    Update Favoris.jsx

--- client/src/pages/Favoris.jsx
+++ client/src/pages/Favoris.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import FavorisItem from "../components/FavorisItem";
 import FavoriteContext from "../contexts/FavoriteContext";
