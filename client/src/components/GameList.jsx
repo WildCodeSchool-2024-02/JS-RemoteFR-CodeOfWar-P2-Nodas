65e2a37f28ab65186363c5952c78a5a6a1063bc1
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-07-18 01:46:19 +0200

    more css fixes, new link in gamelist to enable route to gamepage, replaced css for img due to this change

--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -1,14 +1,23 @@
+import { Link} from "react-router-dom"
 import PropTypes from "prop-types";
 
 export default function GameList ({ games }) {
+  
   return (
     <div>
       <h2 className="result-side-title">Jeux trouvés</h2>
       <ul className="result-side-games-list">
         {games.map(game => (
           <li key={game.id} className="game-item">
-            <img src={game.background_image} alt={game.name}/>
-            <h3>{game.name}</h3>
+                 <Link
+                style={{
+                  backgroundImage: `url(${game.background_image})`,
+                  zIndex: 1,
+                }}
+                to={`/gamepage/${game.id}`}
+              >
+                {game.name}
+              </Link>
           </li>
         ))}
       </ul>
