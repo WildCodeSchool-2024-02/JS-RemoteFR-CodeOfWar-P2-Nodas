c23ebabbe3d5a85055b18c493b074ad4c4f63a2d
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-07-17 23:22:56 +0200

    created filter component to be inside collapsable component

---
+++ client/src/components/FilterItem.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+export default function FilterItem ({ title, filters, onChange }) {
+  return (
+    <div className="filter-section">
+      <p>{title}</p>
+      {filters.map((filterItem) => (
+        <div key={filterItem.id} className="checkbox-list">
+          <input
+            type="checkbox"
+            id={filterItem.id}
+            name={filterItem.name}
+            value={filterItem.slug || filterItem.id}
+            onChange={onChange}
+          />
+          <label htmlFor={filterItem.slug || filterItem.id}>{filterItem.name}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+FilterItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  filters: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      slug: PropTypes.string,
+    })
+  ).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
