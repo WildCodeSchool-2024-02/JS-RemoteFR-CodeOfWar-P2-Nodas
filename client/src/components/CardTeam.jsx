675b16b0f456b107dd610a7350b27ee2395a4891
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-07-18 15:35:48 +0200

    css overhaul for about.css desktop and css fixed in other files

--- client/src/components/CardTeam.jsx
+++ client/src/components/CardTeam.jsx
@@ -4,11 +4,9 @@ import PropTypes from "prop-types";
 export default function CardTeam({ user }) {
   return (
     <div key={user.id} className="cardContainer">
-      <div>
-        <img src={user.avatar} alt={`Avatar ${user.name}`} />
-      </div>
+      <img src={user.avatar} alt={`Avatar ${user.name}`} />
       <div className="userDetails">
-        <h3 id="about3">{user.name}</h3>
+        <h3 className="team-names">{user.name}</h3>
         <ul className="listUser">
           <li>Age : {user.age} years old</li>
           <li>Description : {user.description}</li>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,39 +6,41 @@ import networkLink from "../data/dataNetwork";
 
 export default function About() {
   return (
-    <>
-      <h1 id="about1"> Noda Games</h1>
+    <div className="aboutus">
+      <h1 className="crew_name">Noda Games</h1>
       <div className="descriptionName">
         <ul>
           <li>
-            <b>N</b>ew <b>O</b>bjectif <b>D</b>éveloppement <b>A</b>venture
+            <b>N</b>ew <b>O</b>bjective <b>D</b>evelopment <b>A</b>venture
           </li>
           <li>
-            Explore, discover, play: NODA revolutionizes your gaming experience!
+            Explore, discover, play: <b>NODA</b> revolutionizes your gaming
+            experience!
           </li>
         </ul>
       </div>
-      <div className="bgImage">
-        <img src={gamer} alt="img" className="gif" />
+      <img src={gamer} alt="img" className="gif" />
+      <div className="nodas">
+        <section className="story-network">
+          <h2 className="Our_story second_title">Our story</h2>
+          <p className="textAbout">
+            A team of gamers enthusiasts has created a games site. Together,
+            they combined their skills to design and implement an exceptional
+            user experience. Together, they are redefining video game discovery!
+          </p>
+          <h2 className="follow-us second_title">Follow us</h2>
+          <article className="network_link">
+            {networkLink.map((network) => (
+              <SocialNetwork key={network} network={network} />
+            ))}
+          </article>
+        </section>
+        <section className="avatar">
+          {team.map((user) => (
+            <CardTeam key={user} user={user} />
+          ))}
+        </section>
       </div>
-      <h2 id="about2">Our story</h2>
-      <p className="textAbout">
-        A team of gamers enthusiasts has created a games site. Together, they
-        combined their skills to design and implement an exceptional user
-        experience. Together, they are redefining video game discovery!
-      </p>
-      <section className="avatar">
-        {team.map((user) => (
-          <CardTeam key={user} user={user} />
-        ))}
-      </section>
-
-      <h2 id="about4">Follow us</h2>
-      <article className="network_link">
-        {networkLink.map((network) => (
-          <SocialNetwork key={network} network={network} />
-        ))}
-      </article>
 
       <p className="copyright">
         Noda Gaming © 2024 -{" "}
@@ -50,6 +52,6 @@ export default function About() {
           CGV
         </a>
       </p>
-    </>
+    </div>
   );
 }
